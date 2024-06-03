d91ca14f050bed673e4e9f60a742d0d475115f0b
Balystick <balystick74@icloud.com>
2024-06-03 15:51:39 +0200

    groups fix

---
+++ Challengo/Models/data.swift
@@ -0,0 +1,8 @@
+//
+//  data.swift
+//  Challengo
+//
+//  Created by Apprenant 143 on 03/06/2024.
+//
+
+import Foundation
---
+++ Challengo/Utils/utils.swift
@@ -0,0 +1,8 @@
+//
+//  utils.swift
+//  Challengo
+//
+//  Created by Apprenant 143 on 03/06/2024.
+//
+
+import Foundation
