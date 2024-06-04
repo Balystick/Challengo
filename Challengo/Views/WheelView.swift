4dd6f0041a01f621b9b065e061fcb6b1459bcea3
Balystick <balystick74@icloud.com>
2024-06-04 09:56:10 +0200

    gitignore

---
+++ Challengo/Views/WheelView.swift
@@ -0,0 +1,18 @@
+//
+//  WheelView.swift
+//  Challengo
+//
+//  Created by Apprenant 143 on 04/06/2024.
+//
+
+import SwiftUI
+
+struct WheelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WheelView()
+}
