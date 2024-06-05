88b5e19f056ec1d6cb0f3fdd2b05109a751b25ae
Balystick <balystick74@icloud.com>
2024-06-05 11:17:35 +0200

    Win / Fail Chart

--- Challengo/Views/TrackingBalanceExtractedView.swift
+++ Challengo/Views/TrackingBalanceExtractedView.swift
@@ -6,11 +6,58 @@
 //
 
 import SwiftUI
+import Charts
+
+struct Category: Identifiable {
+    let id = UUID()
+    let title: String
+    let number: Int
+}
 
 struct TrackingBalanceExtractedView: View {
+    @State private var category: [Category] = [
+        .init(title: "Courage existentiel", number: 53),
+        .init(title: "Ouverture à l”expérience et au changement          ", number: 37),
+        .init(title: "Compassion pour soi", number: 28),
+        .init(title: "Autonomie", number: 9),
+        .init(title: "Conscience de soi", number: 17),
+        .init(title: "Compassion pour les autres", number: 13),
+        .init(title: "Responsabilité de soi", number: 23),
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Chart(category) { Category in
+            SectorMark(
+                angle: .value(
+                    Text(verbatim: Category.title),
+                    Category.number
+                ),
+                innerRadius: .ratio(0.6),
+                angularInset: 2
+                
+            )
+            .foregroundStyle(
+                by: .value(
+                    Text(verbatim: Category.title),
+                    Category.title
+                )
+            )
+        }
+        .chartLegend(position: .bottom, alignment: .center)
+        .chartBackground { chartProxy in
+            GeometryReader { geometry in
+                if let anchor = chartProxy.plotFrame {
+                    let frame = geometry[anchor]
+                    VStack {
+                        Text("&nbsp;&nbsp;Challenges\naccomplis par\n&nbsp;&nbsp;&nbsp;catégorie")
+                            .position(x: frame.midX, y: frame.midY)
+                    }
+                }
+            }
+        }
+        Spacer()
     }
+
 }
 
 #Preview {
