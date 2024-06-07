f6a9b3d1fab47a71750227a995ff66af41daca83
Audrey <ahda.audrey92@gmail.com>
2024-06-07 08:12:19 +0200

    Ajout elements dans NatureGrowthView

--- Challengo/Views/NatureGrowthView.swift
+++ Challengo/Views/NatureGrowthView.swift
@@ -20,14 +20,54 @@ struct NatureGrowthView: View {
     @State private var startLocation: CGSize = .zero
     // Collection d'items gagnés précédemment
     @State private var items: [NatureItem] = [
+       //Ciel
         NatureItem(position: CGSize(width: 50, height: 50), imageName: "sun1"),
         NatureItem(position: CGSize(width: 100, height: 100), imageName: "nuage1"),
-        NatureItem(position: CGSize(width: 150, height: 150), imageName: "nuage5"),
-        NatureItem(position: CGSize(width: 200, height: 150), imageName: "nuage1"),
-        NatureItem(position: CGSize(width: 250, height: 150), imageName: "nuage1"),
-        NatureItem(position: CGSize(width: 300, height: 150), imageName: "nuage1"),
-        NatureItem(position: CGSize(width: 350, height: 150), imageName: "nuage1"),
-        NatureItem(position: CGSize(width: 400, height: 750), imageName: "tree.fill") // Ajout du nouvel élément gagné
+        NatureItem(position: CGSize(width: 150, height: 250), imageName: "nuage5"),
+        NatureItem(position: CGSize(width: 200, height: 140), imageName: "nuage2"),
+        NatureItem(position: CGSize(width: 250, height: 300), imageName: "nuage3"),
+        NatureItem(position: CGSize(width: 300, height: 300), imageName: "nuage3"),
+        NatureItem(position: CGSize(width: 350, height: 250), imageName: "nuage1"),
+        
+        //Arbres
+        NatureItem(position: CGSize(width: 50, height: 750), imageName: "arbre1"),
+        NatureItem(position: CGSize(width: 60, height: 750), imageName: "arbre1"),
+        NatureItem(position: CGSize(width: 75, height: 750), imageName: "arbre1"),
+        NatureItem(position: CGSize(width: 60, height: 740), imageName: "arbre1"),
+        NatureItem(position: CGSize(width: 150, height: 550), imageName: "arbre5"),
+        NatureItem(position: CGSize(width: 230, height: 550), imageName: "arbre5"),
+        NatureItem(position: CGSize(width: 70, height: 550), imageName: "arbre5"),
+        NatureItem(position: CGSize(width: 80, height: 500), imageName: "arbre5"),
+        NatureItem(position: CGSize(width: 60, height: 500), imageName: "arbre5"),
+        NatureItem(position: CGSize(width: 120, height: 570), imageName: "arbre5"),
+        
+        //Fleurs
+        NatureItem(position: CGSize(width: 350, height: 750), imageName: "buisson1"),
+        NatureItem(position: CGSize(width: 300, height: 750), imageName: "buisson3"),
+        NatureItem(position: CGSize(width: 320, height: 770), imageName: "buisson3"),
+
+        
+        
+        //Autres
+        NatureItem(position: CGSize(width: 350, height: 550), imageName: "lac1"),
+        NatureItem(position: CGSize(width: 300, height: 650), imageName: "cascade"),
+        NatureItem(position: CGSize(width: 220, height: 770), imageName: "pierres"),
+        NatureItem(position: CGSize(width: 180, height: 640), imageName: "lavande"),
+        NatureItem(position: CGSize(width: 190, height: 640), imageName: "lavande"),
+        NatureItem(position: CGSize(width: 200, height: 640), imageName: "lavande"),
+        NatureItem(position: CGSize(width: 180, height: 640), imageName: "lavande"),
+        NatureItem(position: CGSize(width: 180, height: 680), imageName: "lavande"),
+        NatureItem(position: CGSize(width: 180, height: 660), imageName: "lavande"),
+        NatureItem(position: CGSize(width: 180, height: 700), imageName: "lavande"),
+        
+
+        
+        
+        
+        
+        
+        
+        NatureItem(position: CGSize(width: 400, height: 750), imageName: "arbre1") // Ajout du nouvel élément gagné
     ]
     
     let darkGreen = Color(red: 34/255, green: 139/255, blue: 34/255)
@@ -51,9 +91,9 @@ struct NatureGrowthView: View {
                 
                 // Affichage du nouvel item draggable
                 if item.id == items[items.count-1].id  {
-                    Image(systemName: "tree.fill")
+                    Image("arbre1")
                         .resizable()
-                        .frame(width: 75, height: 75)
+                        .frame(width: 50, height: 50)
                         .foregroundColor(darkGreen)
                         .offset(x: offset.width, y: offset.height)
                         .gesture(
