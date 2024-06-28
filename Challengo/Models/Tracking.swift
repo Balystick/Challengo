58c0824bb19b17759f3c136794044c495754f218
Balystick <balystick74@icloud.com>
2024-06-28 13:47:54 +0200

    DocC : Tracking

--- Challengo/Models/Tracking.swift
+++ Challengo/Models/Tracking.swift
@@ -1,5 +1,10 @@
 import SwiftUI
 
+/**
+ Cette enum représente les différentes catégories de challenges.
+ 
+ Chaque catégorie est associée à son nom.
+ */
 enum ChallengeCategory: String, CaseIterable {
     case existentialCourage = "Courage existentiel"
     case opennessToExperience = "Ouverture à l'expérience et au changement"
@@ -10,9 +15,15 @@ enum ChallengeCategory: String, CaseIterable {
     case selfResponsibility = "Responsabilité de soi"
 }
 
+/**
+ Cette enum représente le statut des challenges.
+ 
+ Chaque statut est associé à un titre dans la légende et à une couleur (verte/rouge).
+ */
 enum ChallengeStatus: String, CaseIterable {
     case succeeded = "Challenges réussis"
     case failed = "Challenges échoués"
+    /// Couleur associée au statut du challenge.
     var color: Color {
         switch self {
         case .succeeded:
@@ -23,12 +34,27 @@ enum ChallengeStatus: String, CaseIterable {
     }
 }
 
+/**
+ Classe représentant un challenge avec ses propriétés de suivi.
+ */
 class TrackingChallenge {
-    var id: UUID
-    var category: ChallengeCategory
-    var date: String
-    var status: ChallengeStatus
-    
+    /// Identifiant unique du challenge.
+    let id: UUID
+    /// Catégorie du challenge.
+    let category: ChallengeCategory
+    /// Date du challenge sous forme de chaîne.
+    let date: String
+    /// Statut du challenge.
+    let status: ChallengeStatus
+    /**
+     Initialise un nouveau challenge de suivi.
+     
+     - Parameters:
+        - id: Identifiant unique du challenge.
+        - category: Catégorie du challenge.
+        - date: Date du challenge sous forme de String.
+        - status: Statut du challenge.
+     */
     init(id: UUID = UUID(), category: ChallengeCategory, date: String, status: ChallengeStatus) {
         self.id = id
         self.category = category
@@ -37,20 +63,42 @@ class TrackingChallenge {
     }
 }
 
+/**
+ Classe contenant des outils pour le suivi des challenges.
+ */
 class TrackingTools {
-    // Fonction pour obtenir le nom du mois à partir d'une date
+    /**
+     Cette méthode retourne le nom du mois correspondant à un numéro de mois donné.
+     
+     - Parameter date: Numéro du mois (1 pour janvier, 2 pour février, etc.).
+     - Returns: Nom du mois correspondant en chaîne de caractères.
+     */
     static func monthName(date: Int) -> String {
         let monthNames = ["Jan", "Fev", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
         return monthNames[date-1]
     }
-    // Fonction pour extraire le nom du mois d'une date
+
+    /**
+     Cette méthode extrait le numéro du mois à partir d'une date donnée.
+     
+     - Parameter date: Date sous forme de chaîne de caractères (format "yyyy-mm-ddThh-mm").
+     - Returns: Numéro du mois extrait de la date.
+     */
     static func dateToMonth(date: String) -> Int {
         let start = date.index(date.startIndex, offsetBy: 5)
         let end = date.index(date.startIndex, offsetBy: 7)
-        return (Int(String(date[start..<end]))!) //Déballage forcé ?
+        return Int(String(date[start..<end])) ?? 0
     }
     
-    // Calcul du nombre de Challenges par mois selon le statut
+    /**
+     Cette méthode calcule le nombre de challenges pour un mois donné selon un statut spécifique.
+     
+     - Parameters:
+        - trackingChallenges: Liste des challenges suivis.
+        - month: Numéro du mois.
+        - status: Statut des challenges à compter.
+     - Returns: Nombre de challenges correspondant aux critères.
+     */
     static func statusCount(trackingChallenges: [TrackingChallenge], month: Int, status: ChallengeStatus) -> Int {
         var statusCount: Int = 0
         for challenge in trackingChallenges {
@@ -63,7 +111,12 @@ class TrackingTools {
         return statusCount
     }
     
-    // Construction du tableau winFailChallengeData
+    /**
+     Cette méthode construit le tableau du nombre de challenges réussis et échoués par mois.
+     
+     - Parameter trackingChallenges: Liste des challenges suivis.
+     - Returns: Tableau des données de réussites et d'échecs des challenges par mois.
+     */
     static func buildWinFailChallengeData(trackingChallenges: [TrackingChallenge]) -> [WinFailChallengeData] {
         var winFailChallengeData: [WinFailChallengeData] = []
         for i in 1...12 {
@@ -80,11 +133,16 @@ class TrackingTools {
         return winFailChallengeData
     }
     
-    // Construction du tableau balanceChallengeCat
-    static func buildBalanceChallengeCat(trackingChallenges: [TrackingChallenge]) -> [BalanceChallengeCategory] {
+    /**
+     Cette méthode construit le tableau du nombre de challenges accomplis par catégorie.
+     
+     - Parameter [trackingChallenges]: Tableau des challenges accomplis.
+     - Returns: Tableau du nombre de challenges accomplis par catégorie.
+     */
+        static func buildBalanceChallengeCat(trackingChallenges: [TrackingChallenge]) -> [BalanceChallengeCategory] {
         var balanceChallengeCat: [BalanceChallengeCategory] = []
-        var challengeCount: Int = 0
         for category in ChallengeCategory.allCases {
+            var challengeCount: Int = 0
             for challenge in trackingChallenges {
                 if challenge.category == category {
                     challengeCount += 1
@@ -95,13 +153,17 @@ class TrackingTools {
         return balanceChallengeCat
     }
 }
-
+/**
+ Cette structure représente le nombre de challenges accomplis dans une catégorie
+ */
 struct BalanceChallengeCategory: Identifiable {
     var id = UUID()
     var title: String
     var number: Int
 }
-
+/**
+ Cette structure représente le nombre de challenges réussis et échoués par mois.
+ */
 struct WinFailChallengeData: Identifiable {
     var id = UUID()
     var month: String
@@ -109,7 +171,7 @@ struct WinFailChallengeData: Identifiable {
     var challengeStatus: ChallengeStatus
 }
 
-
+// Tableau des challenges accomplis
 let trackingChallenges: [TrackingChallenge] = [
     TrackingChallenge(category: .compassionForOthers, date: "2024-01-02T00-00", status: .failed),
     TrackingChallenge(category: .existentialCourage, date: "2024-01-03T00-00", status: .failed),
@@ -212,8 +274,10 @@ let trackingChallenges: [TrackingChallenge] = [
     TrackingChallenge(category: .existentialCourage, date: "2024-04-09T00-00", status: .failed),
     TrackingChallenge(category: .autonomy, date: "2024-04-10T00-00", status: .succeeded)]
 
+// Génération du tableau représentant le nombre de challenges réussis et échoués par mois.
 let winFailChallengeDatas = TrackingTools.buildWinFailChallengeData(trackingChallenges: trackingChallenges)
 
+// Génération du tableau représentant le nombre de challenges accomplis par catégorie
 let balanceChallengeCat = TrackingTools.buildBalanceChallengeCat(trackingChallenges: trackingChallenges)
 
 
